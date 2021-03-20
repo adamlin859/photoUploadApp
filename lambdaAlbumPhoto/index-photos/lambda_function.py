10a9f623cc30019d72c0a1dd28a0b64a76a1a27a
Adam <ycl2112@columbia.edu>
2021-03-19 23:18:37 -0400

    Inital template for app

---
+++ lambdaAlbumPhoto/index-photos/lambda_function.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+import botocore
+from datetime import datetime
+from get_rekog_labels import get_rekog_labels
+import requests
+from requests.auth import HTTPBasicAuth
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3', region_name='us-east-1')
+
+    s3_info = event['Records'][0]['s3']
+    bucket = s3_info['bucket']['name']
+    name = s3_info['object']['key']
+
+    # extract es endpoint information
+    es_client = boto3.client('es')
+    es_endpoint = client.describe_elasticsearch_domain(DomainName='photos-cf')['DomainStatus']['Endpoint']
+
+    # Extract custom labels
+    metadata_resp = s3_client.head_object(Bucket = bucket, Key = name)
+    custom_label = metadata_resp['Metadata']['customlabels']
+    custom_label = [x.strip().lower() for x in custom_label.split(',')]
+
+
+    # Extract rekog labels
+    labels = get_rekog_labels(bucket, name)
+    labels.extend(custom_label)
+    labels = list(set(labels))
+    print(labels)
+
+    time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4])
+
+    es_dict = {
+        "objectKey": name,
+        "bucket": bucket,
+        "createdTimestamp": time,
+        "labels": labels
+    }
+
+
+    post_request_url = 'https://' + es_endpoint + '/photos/photo'
+    r = requests.post(post_request_url, auth=HTTPBasicAuth('coms6998', 'Coms6998!'), json=es_dict)
+
+    print(r)
+
+    return {
+        'statusCode': 200,
+        'body': 'Photo has been added to the index'
+    }
---
+++ lambdaAlbumPhoto/search-photos/lambda_function.py
@@ -0,0 +1,62 @@
+import json
+import boto3
+import requests
+from requests.auth import HTTPBasicAuth
+​
+def lambda_handler(event, context):
+​    bucket_url = 'https://photos-bucket-cf.s3.amazonaws.com/'
+    query = event['queryStringParameters']['q'].strip().lower()
+
+    # extract es endpoint information
+    es_client = boto3.client('es')
+    es_endpoint = client.describe_elasticsearch_domain(DomainName='photos_cf')['DomainStatus']['Endpoint']
+​    # Query lex bot
+    client = boto3.client('lex-runtime', region_name='us-east-1')
+    response = client.post_text(
+        botName='search_photos',
+        botAlias='prod',
+        userId='cesare',
+        inputText=query
+    )
+
+    # If no slots are filled return an empty array
+    if not response.get('slots', False):
+        #return empty array of photos
+        return {
+            'statusCode': 200,
+            'body': {}
+        }
+
+    slots = response['slots']
+​    labels = []
+    for k, v in slots.items():
+        if v:
+            labels.append(v)
+
+    # Search labels
+    photo_keys = {}
+    photo_keys['result'] = []
+    for lab in labels:
+        # print(lab)
+        get_request_url = 'https://' + es_endpoint + '/photos/_search?q='
+        es_response = requests.get(get_request_url+lab, auth=HTTPBasicAuth('coms6998', 'Coms6998!'))
+        es_search = json.loads(es_response.content.decode('utf-8'))
+        # print(es_search)
+        hits = es_search['hits']['hits']
+        # print('h', hits)
+        if len(hits)==0:
+            continue
+
+        for photo in hits:
+            print(photo)
+            photo_keys['result'].append({'url': bucket_url + photo['_source']['objectKey'], "labels":[lab]})
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        'body':  json.dumps(photo_keys)
+    }
